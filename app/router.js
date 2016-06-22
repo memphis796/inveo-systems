ae395672bb6acdaebd0fa7409e26147883488449
Memphis796 <memphis796@Hotmail.com>
2016-06-22 01:02:06 -0400

    marked up router

--- app/router.js
+++ app/router.js
@@ -6,16 +6,13 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-<<<<<<< HEAD
   this.route('register');
-=======
   this.route('order');
   this.route('logout');
   this.route('login');
   this.route('home');
   this.route('checkout');
   this.route('about');
->>>>>>> d2d5a5ca659bdbebf205321e5bc864fe7f3fa8b0
 });
 
 export default Router;

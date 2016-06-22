e7c727bcbe60ca1a3c066c8d6e4659f69d762ae0
Memphis796 <memphis796@Hotmail.com>
2016-06-21 23:52:48 -0400

    markup router

--- app/router.js
+++ app/router.js
@@ -7,13 +7,6 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about');
-  this.route('home');
-  this.route('login');
-  this.route('logout');
-  this.route('register');
-  this.route('reviews');
-  this.route('checkout');
-  this.route('order');
 });
 
 export default Router;

83689903f4856885151f34556be14e5518c5ba78
Memphis796 <memphis796@Hotmail.com>
2016-06-25 00:39:45 -0400

    fixed deployment

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    apiHost: 'http://localhost:3000',
+    apiHost: 'http://inveo-system.herokuapp.com',
 
     modulePrefix: 'inveo-systems',
     environment: environment,

9697f87fa01c1daff0983143a9f7b153ca2f4709
Memphis796 <memphis796@Hotmail.com>
2016-07-29 13:55:33 -0500

    marked up index fully

--- app/index/login/controller.js
+++ app/index/login/controller.js
@@ -4,8 +4,7 @@ export default Ember.Controller.extend({
   session: Ember.inject.service(),
 
   login(formValues) {
-    this.get(`session`).authenticate(
-      `authenticator:application`, formValues.email, formValues.password)
+    this.get(`session`).authenticate(`authenticator:application`,formValues.email, formValues.password)
     .then(() => {
       console.log(`logged in`);
       this.transitionToRoute(`home`);
